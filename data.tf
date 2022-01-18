63d2eccc9c1d418d9b63a1b2dc6298bcefb682ab
Sergey Novikov <sergey.novikov@flaconi.de>
2022-01-18 18:46:28 +0100

    Remove unused data source

--- data.tf
+++ data.tf
@@ -1,7 +1,3 @@
-data "github_organization" "this" {
-  name = var.owner
-}
-
 data "github_repository" "managed" {
   for_each  = toset(flatten([for k, v in var.secrets : v.repositories if v.repositories != null]))
   full_name = "${var.owner}/${each.value}"
