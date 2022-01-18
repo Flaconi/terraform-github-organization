99f6b93fb0935b74021c94def2e593157b55e923
Sergey Novikov <sergey.novikov@flaconi.de>
2022-01-18 17:28:23 +0100

    Add secrets management

---
+++ main.tf
@@ -0,0 +1,9 @@
+resource "github_actions_organization_secret" "this" {
+  for_each = var.secrets
+
+  secret_name             = each.key
+  visibility              = each.value.visibility
+  encrypted_value         = lookup(each.value, "encrypted_value", null)
+  plaintext_value         = lookup(each.value, "plaintext_value", null)
+  selected_repository_ids = each.value.visibility == "selected" ? [for r in lookup(each.value, "repositories", []) : data.github_repository.managed[r].repo_id] : []
+}
--- variables.tf
+++ variables.tf
@@ -7,3 +7,14 @@ variable "owner" {
   description = "Github organization name to manage."
   type        = string
 }
+
+variable "secrets" {
+  description = "Global organization secrets"
+  type = map(object({
+    encrypted_value = optional(string)
+    plaintext_value = optional(string)
+    visibility      = string # "all", "private" or "selected"
+    repositories    = optional(set(string))
+  }))
+  default = {}
+}
