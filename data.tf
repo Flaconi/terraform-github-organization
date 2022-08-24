9831a36361d0d4a7a4663a96bdf43ef00ec240d6
Sergey Novikov <sergey.novikov@flaconi.de>
2022-08-24 13:54:44 +0200

    Fix missing repository associations for Dependabot

--- data.tf
+++ data.tf
@@ -3,6 +3,9 @@ data "github_organization" "this" {
 }
 
 data "github_repository" "managed" {
-  for_each  = toset(flatten([for k, v in var.secrets : v.repositories if v.repositories != null]))
+  for_each  = toset(distinct(flatten(concat(
+    [for k, v in var.secrets : v.repositories if v.repositories != null],
+    [for k, v in var.bot_secrets : v.repositories if v.repositories != null],
+  ))))
   full_name = "${data.github_organization.this.login}/${each.value}"
 }
